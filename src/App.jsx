4aadab56babf3a918d99d6f50a0d4ef4a85a350e
ham-zi <ddmt0805@gmail.com>
2026-08-06 11:12:21 +0900

    소셜로그인 성공 시 루트 설정

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,7 @@ export default function App() {
           <Route index element={<HomePage />} />
           <Route path="/auth/login" element={<LoginPage />} />
           <Route path="/auth/signup" element={<SignupPage />} />
+          <Route path="/auth/oauth-success" element={<HomePage />} />
           <Route path="/oauth2/success" element={<OAuthSuccessPage />} />
           <Route
             path="/error-wiki"
